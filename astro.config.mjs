325b666cfc9c4ddb53b9d7d1b1c17e18f85ad9ee
Craig McKeachie <craigdmckeachie@gmail.com>
2024-04-07 20:27:16 -0400

    git content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,37 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "Code Academy",
+
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Git",
+          items: [
+            { label: "Getting Started", link: "/git/getting-started" },
+            { label: "Configuring Git", link: "/git/configuration" },
+            { label: "Using Git in VS Code", link: "/git/git-vscode" },
+            { label: "Using Git on the Command-Line", link: "/git/git-command-line" },
+          ],
+        },
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", link: "/guides/example/" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
